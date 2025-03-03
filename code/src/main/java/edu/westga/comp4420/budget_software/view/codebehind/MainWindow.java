4f64a89a99bed341c0b4e2576cdd6b03bc022789
Nick Watts <>
2025-03-03 14:18:49 -0500

    UI and Codebehind Created
    1.) Created the UI for the budget software.
    
    2.) Filled in the codebehind with the UI elements.

--- code/src/main/java/edu/westga/comp4420/budget_software/view/codebehind/MainWindow.java
+++ code/src/main/java/edu/westga/comp4420/budget_software/view/codebehind/MainWindow.java
@@ -1,5 +1,10 @@
 package edu.westga.comp4420.budget_software.view.codebehind;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
 
 /**
  * CodeBehind To Handle Processing for the MainWindow
@@ -8,5 +13,18 @@ package edu.westga.comp4420.budget_software.view.codebehind;
  * @version Spring 2024
  */
 public class MainWindow {   
+    @FXML
+    private Button buttonAddExpense;
 
+    @FXML
+    private Button buttonPersonalInfo;
+
+    @FXML
+    private Button buttonSaveBudget;
+
+    @FXML
+    private ListView<?> listviewExpenses;
+
+    @FXML
+    private TextArea textAreaStats;
 }
