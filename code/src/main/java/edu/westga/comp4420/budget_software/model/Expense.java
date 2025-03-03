7fca5f059b3880ac1b5f89f2b918ce9fa32a9770
Nick Watts <>
2025-03-03 15:12:02 -0500

    Created Model Code
    1.) Added an Expense class to contain an expense.
    
    2.) Added an Expenses class to contain multiple expenses within a
    ListProperty for binding.
    
    3.) Added a delete expense button to the UI

---
+++ code/src/main/java/edu/westga/comp4420/budget_software/model/Expense.java
@@ -0,0 +1,64 @@
+package edu.westga.comp4420.budget_software.model;
+
+public class Expense {
+    private static final String ARGUMENT_NAME_CANNOT_BE_BLANK = "Argument: name cannot be blank";
+    private static final String ARGUMENT_AMOUNT_CANNOT_BE_BLANK = "Argument: amount cannot be blank";
+    private static final String ARGUMENT_CATEGORY_INVALID = "Argument: category invalid.";
+
+    private String name;
+    private float amount;
+    private Category category;
+
+    /**
+     *  Constructor for the expense class that takes
+     * a name and amount. Sets the category to misc.
+     * @param name The name of the expense
+     * @param amount The amount the expense costs
+     */
+    public Expense(String name, float amount) {
+        this.checkConstructorInput(name, amount);
+        this.name = name;
+        this.amount = amount;
+        this.category = Category.MISC;
+    }
+
+    /**
+     * The expense constructor that allows setting a category
+     * Takes the name, amount, and category.
+     * @param name name of the expense
+     * @param amount the amount the expense costs
+     * @param category the category of the expense
+     */
+    public Expense(String name, float amount, Category category) {
+        this.checkConstructorInput(name, amount, category);
+        this.name = name;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    private void checkConstructorInput(String name, float amount) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException(ARGUMENT_NAME_CANNOT_BE_BLANK);
+        } else if (amount == 0) {
+            throw new IllegalArgumentException(ARGUMENT_AMOUNT_CANNOT_BE_BLANK);
+        }
+    }
+
+    private void checkConstructorInput(String name, float amount, Category category) {
+        boolean valid_enum = false;
+
+        for (Category cat : Category.values()) {
+            if (category == cat) {
+                valid_enum = true;
+            }
+        }
+
+        if (name.isBlank()) {
+            throw new IllegalArgumentException(ARGUMENT_NAME_CANNOT_BE_BLANK);
+        } else if (amount == 0) {
+            throw new IllegalArgumentException(ARGUMENT_AMOUNT_CANNOT_BE_BLANK);
+        } else if (!valid_enum) {
+            throw new IllegalArgumentException(ARGUMENT_CATEGORY_INVALID);
+        }
+    }
+}
---
+++ code/src/main/java/edu/westga/comp4420/budget_software/model/Expenses.java
@@ -0,0 +1,35 @@
+package edu.westga.comp4420.budget_software.model;
+
+import java.util.ArrayList;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+
+public class Expenses {
+
+    private ListProperty<Expense> expenses;
+
+    public Expenses() {
+        this.expenses = new SimpleListProperty<Expense>(FXCollections.observableArrayList(new ArrayList<Expense>()));
+    }
+
+    /**
+     * Add an expense to the expenses.
+     * @param expense the expense to add
+     */
+    public void addExpense(Expense expense) {
+        this.expenses.add(expense);
+    }
+
+    /**
+     * Remove an expense from the expeneses
+     * @param expense The expense to remove
+     */
+    public void removeExpense(Expense expense) {
+        if (!this.expenses.contains(expense)) {
+            return;
+        }
+        this.expenses.remove(expense);
+    }
+}
